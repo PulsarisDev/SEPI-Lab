6813ce232b29fd77783eda5c41d55fcfa45ad8f2
WU Shuo <s.wu@bupt.edu.cn>
2024-03-30 22:25:41 +0800

    message 2: the first functional_test

--- functional_test.py
+++ functional_test.py
@@ -1,14 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.service = Service('msedgedriver')
+        self.service.start()
+        self.driver = webdriver.Remote(self.service.service_url)
+        return super().setUp()
+    
+    def tearDown(self) -> None:
+        self.driver.quit()
+        return super().tearDown()
+    
+    def test_1(self) -> None:
+        self.driver.get('http://localhost:8000/')
+
+        self.assertIn('To-Do', self.driver.title)
+        self.fail("Finished the test!")
 
 
 if __name__ == "__main__":
-    service = Service('msedgedriver')
-    service.start()
-    driver = webdriver.Remote(service.service_url)
-
-    driver.get('http://localhost:8000/')
-
-    assert 'Django' in driver.page_source
-
-    driver.quit()
+    unittest.main()
