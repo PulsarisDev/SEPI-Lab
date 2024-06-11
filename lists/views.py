c3e776a4ab9ad4f942d656fa3f282a8a37ea5ef2
WU Shuo <s.wu@bupt.edu.cn>
2024-06-11 12:08:12 +0800

    message 17: change models for multiple users

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request: HttpRequest):
     return render(request, 'home.html')
@@ -10,5 +10,6 @@ def view_list(request: HttpRequest):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request: HttpRequest):
-    Item.objects.create(text = request.POST['item_text'])
+    list_user = List.objects.create()
+    Item.objects.create(text = request.POST['item_text'], list = list_user)
     return redirect('/lists/the-new-page/')
