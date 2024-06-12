5ac6b75b26d87869458fe830def3d6408fd29dc1
WU Shuo <s.wu@bupt.edu.cn>
2024-06-13 03:25:36 +0800

    message 26: add gunicorn to requirements.txt; slightly tweak settings

--- DjangoDemo/settings.py
+++ DjangoDemo/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4*r_-^-ecv=f@h0vjwai&_zgkwox#+s!s-_o=9g$tu#j^g24r_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['45.153.129.129']
 
 
 # Application definition
